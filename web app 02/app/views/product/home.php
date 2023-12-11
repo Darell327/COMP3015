a75e67f7cb58d0ccfbd77f48184dd889583e6af1
Darell327 <145581712+Darell327@users.noreply.github.com>
2023-12-10 23:45:26 -0400

    Add files via upload

---
+++ web app 02/app/views/product/home.php
@@ -0,0 +1,11 @@
+<h1>Bienvenido a Nuestra Aplicación</h1>
+<nav>
+<ul>
+<li><a href=".">Ver Usuarios</a></li>
+16
+<!-- Más enlaces de navegación según sea necesario -->
+</ul>
+<ul>
+<li><a href="./index.php?url=product/index">Ver Productos</a></li>
+</ul>
+</nav>
